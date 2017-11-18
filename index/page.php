83955c2016d0629e3f7a2adc3611585d7906b6b3
shadebane <shadebane96@gmail.com>
2017-11-17 22:55:17 -0600

    a test

--- index/page.php
+++ index/page.php
@@ -1,7 +1,8 @@
 <?php
+  
   $HEAD = file_get_contents("./html/head.html");
   $BODY = file_get_contents("./html/body.html");
-if (empty($HEAD)) {
-  $HEAD = "aaaa";
+if (empty($BODY)) {
+  $BODY = realpath(dirname(__FILE__));
 }
 ?>
