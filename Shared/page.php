a4d9be78b0d32ea36e69ea5c996fa6b99ff48dbd
shadebane <shadebane96@gmail.com>
2017-11-18 11:27:18 -0600

    Adding top bar with no color yet

--- Shared/page.php
+++ Shared/page.php
@@ -1,5 +1,6 @@
 <?php
-  $filepath = realpath(dirname(__FILE__));
   $SHARED = array(); 
-  $SHARED['includes.html'] = file_get_contents("$filepath/html/includes.html");
+  $SHARED['HTML_includes'] = file_get_contents(__DIR__ . "/html/includes.html");
+  $SHARED['HTML_topbar'] = file_get_contents(__DIR__ . "/html/topbar.html");
+  $SHARED['CSS_topbar'] = "<link rel='stylesheet' href='./Shared/css/topbar.css'>";
 ?>
--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
-$filepath = realpath(dirname(__FILE__));
-global _SHARED_PATH_ = "$filepath/Shared/page.php";
-require _SHARED_PATH_;
-$pageName = "./index";
-require "$filepath/$pageName/page.php";
+// this sets the inclues path to here
+$pageName = "index";
+include_once __DIR__ . "/Shared/page.php";
+include_once __DIR__ . "/$pageName/page.php";
 echo 
 "<html>
   <head>";
-if (!empty($SHARED['includes.html']))
-  echo $SHARED['includes.html'];
-if (!empty($HEAD)) {
-  echo $HEAD;
-}
+if (!empty($SHARED['CSS_topbar']))
+  echo $SHARED['CSS_topbar'];
+if (!empty($SHARED['HTML_includes']))
+  echo $SHARED['HTML_includes'];
+if (!empty($CONTENT['HEAD']))
+  echo $CONTENT['HEAD'];
 echo 
 " </head>
   <body>";
-if (!empty($BODY)) {
-  echo $HEAD;
-}
+if (!empty($SHARED['HTML_topbar']))
+  echo $SHARED['HTML_topbar'];
+if(!empty($CONTENT['BODY']))
+  echo $CONTENT['BODY'];
 echo
 " </body>
 </html>";
