9c1c63c4169d3742097e45e8bbd4b930be9dcfde
shadebane <shadebane96@gmail.com>
2017-11-22 21:57:40 -0600

    Trying to fix dependencies

--- Shared/page.php
+++ Shared/page.php
@@ -3,10 +3,10 @@
   $SHARED = array();
   $SHARED['HTML_includes'] = file_get_contents(__DIR__ . "/html/includes.html");
   $SHARED['HTML_topbar'] = file_get_contents(__DIR__ . "/html/topbar.html");
-  $SHARED['CSS'] = "<link rel='stylesheet' href='/Shared/css/topbar.css'>";
-  $SHARED['JS'] = "<script src='/Shared/js/topbar.js'></script>";
-  $SHARED['CSS'] .= "<link rel='stylesheet' href='/Shared/css/sidebar.css'>";
-  $SHARED['JS'] .= "<script src='/Shared/js/sidebar.js'></script>";
-  $SHARED['JS'] .= "<script src='/Shared/js/dropdown.js'></script>";
-  $SHARED['JS'] .= "<script src='/Shared/js/scrollToPageId.js'></script>";
+  $SHARED['CSS'] = "<link rel='stylesheet' href='./Shared/css/topbar.css'>";
+  $SHARED['JS'] = "<script src='./Shared/js/topbar.js'></script>";
+  $SHARED['CSS'] .= "<link rel='stylesheet' href='./Shared/css/sidebar.css'>";
+  $SHARED['JS'] .= "<script src='./Shared/js/sidebar.js'></script>";
+  $SHARED['JS'] .= "<script src='./Shared/js/dropdown.js'></script>";
+  $SHARED['JS'] .= "<script src='./Shared/js/scrollToPageId.js'></script>";
 ?>
