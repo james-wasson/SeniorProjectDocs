86dd18163caf32d3612905769f608f592642c268
shadebane <shadebane96@gmail.com>
2017-12-01 03:28:56 -0600

    asd

--- sendEmail.php
+++ sendEmail.php
@@ -14,24 +14,12 @@
       $subject = $_GET["emailSubject"];
     $subject .= "\t" . date("Y-m-d h:i:sa");
     
-    $charset = 'utf-8';
-    $mail     = 'no-reply@'.str_replace('www.', '', $_SERVER['SERVER_NAME']);
-    $uniqid   = md5(uniqid(time()));
-    $headers  = 'From: '.$mail."\n";
-    $headers .= 'Reply-to: '.$mail."\n";
-    $headers .= 'Return-Path: '.$mail."\n";
-    $headers .= 'Message-ID: <'.$uniqid.'@'.$_SERVER['SERVER_NAME'].">\n";
-    $headers .= 'MIME-Version: 1.0'."\n";
-    $headers .= 'Date: '.gmdate('D, d M Y H:i:s', time())."\n";
-    $headers .= 'X-Priority: 3'."\n";
-    $headers .= 'X-MSMail-Priority: Normal'."\n";
-    $headers .= 'Content-Type: multipart/mixed;boundary="----------'.$uniqid.'"'."\n\n";
-    $headers .= '------------'.$uniqid."\n";
-    $headers .= 'Content-type: text/'.$type.';charset='.$charset.''."\n";
-    $headers .= 'Content-transfer-encoding: 7bit';
+    $headers = 'From: noreply@page.com' . "\n" .
+    'Reply-To: noreply@page.com' . "\n" .
+    'X-Mailer: PHP/' . phpversion();
       
     if(mail($CONTENT['EMAIL'], $subject, $msg, $headers)) { // mail sent
-      echo "Mail Sent!" . $CONTENT['EMAIL'];
+      echo "Mail Sent!";
       return http_response_code(200);
     } else { // failed sending mail
       echo "Error, Mail Sent But Not Delivered.";
