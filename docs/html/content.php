45dfe6380daecf7bc36aca618a9f28f00d34230f
shadebane <shadebane96@gmail.com>
2017-12-11 04:13:55 -0600

    adding the first version to the docs page

--- docs/html/content.php
+++ docs/html/content.php
@@ -1,114 +1,107 @@
 <?php
+  $DOC_PAGE_CONTENT;
   function getContentById(&$headings) {
-    global $DOC_PAGE_CONTENT;
-    $cont = array();
-    for ($i = 0; $i < count($headings); $i++) {
-      $h = $headings[$i];
-      if (!empty($DOC_PAGE_CONTENT[$h])) {
-        $cont[$h]['content'] = $DOC_PAGE_CONTENT[$h];
-        $cont[$h]['title'] = $h;
-        $cont[$h]['id'] = $i;
-      } else {
-        unset($cont[$h]);
-      }
-    }
-    $headings = $cont;
-    return formatDocsPageContent($cont);
-  }
-
-  function formatDocsPageContent($headings) {
-    $returnStr = "<div class='container'>";
-    foreach ($headings as  $h) {
-      $content = $h['content'];
-      $id = $h['id'];
-      $title = $h['title'];
-      $text = $h['content'][0]['text'];
-      $version = $h['content'][0]['version'];
-      $returnStr .= "<div class='row'>
-      <div class='section center container' id='content-$id'>
-          <div class='row'>
-            <div class='heading col'>
-              <span class='title'>$title</span>
-              <span class='current-version'>Version: $version</span>
-            </div>
-            <div class='info col'>
-              $text
-            </div>";
-      // if there are older versions
-      if (count($content) > 1) {
-        $returnStr .= "<div class='view-older-versions dropdownButton' data-toggle='old-$id'>View Older Versions <span class='fa fa-caret-down' aria-hidden='true'></span></div>";
-        $returnStr .= "<div id='old-$id' class='old-versions' style='display: none;'>
-        <div class='container'>";
-        foreach ($content as $key => $c) {
-          if ($key != 0) {
-            $version = $c['version'];
-            $text= $c['text'];
-            $returnStr .= "
-            <div class='row'>
-              <div class='col'>
-                <div class='old-version-heading'>
-                  <span class='old-version-tag'>Version: $version</span>
-                </div>
-                <div class='info old-info'>
-                  $text
-                </div>
-              </div>
-            </div>";
+    $displayedContent = "";
+    foreach ($headings as $majorH => $minorHeadings) {
+      $majorIndex = array_search($majorH ,array_keys($headings));
+      if (!is_null($minorHeadings)) {
+        // display the major heading
+        $displayedContent .= "<div class='container'>";
+        $majorContent = array();
+        $majorContent['title'] = $majorH;
+        $majorContent['id'] = $majorIndex;
+        $displayedContent .= formatMajorContent($majorContent);
+        // display the inner content for the major heading
+        foreach ($minorHeadings as $minorIndex => $minorH) {
+          $minorContent = array();
+          $minorContent['content'] = getUnrefinedDocumentContent($minorH);
+          if (!is_null($minorContent['content'])) {
+            $minorContent['title'] = $minorH;
+            $minorContent['id'] = $majorIndex . '-' . $minorIndex;
+            $displayedContent .= formatMinorContent($minorContent);
+          } else {
+            unset($headings[$majorH][$minorIndex]);
           }
         }
-        $returnStr .= "</div>
-        </div>
-        </div>";
+        $displayedContent .= "</div>";
+      } else {
+        unset($headings[$majorH]);
       }
-      // end if there are older versions
-      $returnStr .="</div>
-      </div>";
     }
-    $returnStr .= "</div>";
-    return $returnStr;
+    return $displayedContent;
   }
 
-  $DOC_PAGE_CONTENT = array(
-  "h1"=> array(
-    array(
-    "text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend, tellus sit amet malesuada condimentum, enim eros vestibulum velit, sit amet venenatis quam nibh ut magna. Pellentesque nisi dolor, interdum vel lobortis quis, egestas et magna. In porta pharetra lacus hendrerit fringilla. Vivamus imperdiet nunc vel sem aliquam feugiat. Phasellus pulvinar ac dolor vitae interdum. Vivamus in arcu sed risus ultrices tempus id nec augue. Sed consequat odio neque, eu ullamcorper nisl pharetra non. In eget efficitur elit.",
-    "version" => "1.0.0"),
-    array(
-    "text" => "Curabitur sit amet dolor in augue bibendum dignissim. Nulla tortor velit, rutrum a auctor ut, cursus ac nisl. Ut ut ligula vel metus luctus vestibulum sit amet quis magna. Praesent finibus nisl vitae lacus ullamcorper suscipit. Fusce mattis a neque a fringilla. Praesent sed tincidunt sem, gravida porttitor enim. Mauris facilisis justo ex, vel molestie eros rhoncus vel. Praesent bibendum pretium condimentum. Sed porta ex ex, non semper metus posuere id. Fusce congue luctus enim ac bibendum. Fusce sit amet felis egestas, porttitor eros et, volutpat libero. Morbi gravida iaculis nunc quis rhoncus. Fusce fringilla volutpat ex consequat suscipit.",
-    "version" => "0.1.0"),
-    array(
-    "text" => "Curabitur sit amet dolor in augue bibendum dignissim. Nulla tortor velit, rutrum a auctor ut, cursus ac nisl. Ut ut ligula vel metus luctus vestibulum sit amet quis magna. Praesent finibus nisl vitae lacus ullamcorper suscipit. Fusce mattis a neque a fringilla. Praesent sed tincidunt sem, gravida porttitor enim. Mauris facilisis justo ex, vel molestie eros rhoncus vel. Praesent bibendum pretium condimentum. Sed porta ex ex, non semper metus posuere id. Fusce congue luctus enim ac bibendum. Fusce sit amet felis egestas, porttitor eros et, volutpat libero. Morbi gravida iaculis nunc quis rhoncus. Fusce fringilla volutpat ex consequat suscipit.",
-    "version" => "0.0.1")
-    ),
-  "Hello Sunshine in the rain"=> array(
-    array(
-    "text" => "Quisque non tellus ac dolor tincidunt egestas egestas a ligula. Pellentesque dapibus porta turpis vitae consectetur. Pellentesque vitae lacus convallis, imperdiet dui quis, vehicula urna. Praesent lacinia libero sit amet congue fermentum. Nulla facilisi. Fusce lectus tellus, bibendum eget laoreet id, blandit vitae lorem. Morbi et leo ac ex semper dignissim.",
-    "version" => "1.2.0"),
-    array(
-    "text" => "Mauris eu scelerisque turpis. Etiam vitae diam maximus odio posuere blandit ut nec elit. Fusce rutrum euismod purus, a posuere metus ultrices sed. Aenean venenatis dui augue, a porta justo sodales ut. In id fermentum purus. Proin velit ligula, pharetra vel tincidunt id, condimentum vitae sem. Etiam vel finibus nulla. Pellentesque lobortis aliquet euismod. Sed condimentum erat id quam varius gravida.",
-    "version" => "0.1.0")
-    ),
-  "12345"=> array(
-    array(
-    "text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend, tellus sit amet malesuada condimentum, enim eros vestibulum velit, sit amet venenatis quam nibh ut magna. Pellentesque nisi dolor, interdum vel lobortis quis, egestas et magna. In porta pharetra lacus hendrerit fringilla. Vivamus imperdiet nunc vel sem aliquam feugiat. Phasellus pulvinar ac dolor vitae interdum. Vivamus in arcu sed risus ultrices tempus id nec augue. Sed consequat odio neque, eu ullamcorper nisl pharetra non. In eget efficitur elit.",
-    "version" => "1.0.0"),
-    array(
-    "text" => "Curabitur sit amet dolor in augue bibendum dignissim. Nulla tortor velit, rutrum a auctor ut, cursus ac nisl. Ut ut ligula vel metus luctus vestibulum sit amet quis magna. Praesent finibus nisl vitae lacus ullamcorper suscipit. Fusce mattis a neque a fringilla. Praesent sed tincidunt sem, gravida porttitor enim. Mauris facilisis justo ex, vel molestie eros rhoncus vel. Praesent bibendum pretium condimentum. Sed porta ex ex, non semper metus posuere id. Fusce congue luctus enim ac bibendum. Fusce sit amet felis egestas, porttitor eros et, volutpat libero. Morbi gravida iaculis nunc quis rhoncus. Fusce fringilla volutpat ex consequat suscipit.",
-    "version" => "0.1.0"),
-    array(
-    "text" => "Curabitur sit amet dolor in augue bibendum dignissim. Nulla tortor velit, rutrum a auctor ut, cursus ac nisl. Ut ut ligula vel metus luctus vestibulum sit amet quis magna. Praesent finibus nisl vitae lacus ullamcorper suscipit. Fusce mattis a neque a fringilla. Praesent sed tincidunt sem, gravida porttitor enim. Mauris facilisis justo ex, vel molestie eros rhoncus vel. Praesent bibendum pretium condimentum. Sed porta ex ex, non semper metus posuere id. Fusce congue luctus enim ac bibendum. Fusce sit amet felis egestas, porttitor eros et, volutpat libero. Morbi gravida iaculis nunc quis rhoncus. Fusce fringilla volutpat ex consequat suscipit.",
-    "version" => "0.0.1")
-    ),
-  "Adam Test ##2"=> array(
-    array(
-    "text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend, tellus sit amet malesuada condimentum, enim eros vestibulum velit, sit amet venenatis quam nibh ut magna. Pellentesque nisi dolor, interdum vel lobortis quis, egestas et magna. In porta pharetra lacus hendrerit fringilla. Vivamus imperdiet nunc vel sem aliquam feugiat. Phasellus pulvinar ac dolor vitae interdum. Vivamus in arcu sed risus ultrices tempus id nec augue. Sed consequat odio neque, eu ullamcorper nisl pharetra non. In eget efficitur elit.",
-    "version" => "1.0.0"),
-    array(
-    "text" => "Curabitur sit amet dolor in augue bibendum dignissim. Nulla tortor velit, rutrum a auctor ut, cursus ac nisl. Ut ut ligula vel metus luctus vestibulum sit amet quis magna. Praesent finibus nisl vitae lacus ullamcorper suscipit. Fusce mattis a neque a fringilla. Praesent sed tincidunt sem, gravida porttitor enim. Mauris facilisis justo ex, vel molestie eros rhoncus vel. Praesent bibendum pretium condimentum. Sed porta ex ex, non semper metus posuere id. Fusce congue luctus enim ac bibendum. Fusce sit amet felis egestas, porttitor eros et, volutpat libero. Morbi gravida iaculis nunc quis rhoncus. Fusce fringilla volutpat ex consequat suscipit.",
-    "version" => "0.1.0"),
-    array(
-    "text" => "Curabitur sit amet dolor in augue bibendum dignissim. Nulla tortor velit, rutrum a auctor ut, cursus ac nisl. Ut ut ligula vel metus luctus vestibulum sit amet quis magna. Praesent finibus nisl vitae lacus ullamcorper suscipit. Fusce mattis a neque a fringilla. Praesent sed tincidunt sem, gravida porttitor enim. Mauris facilisis justo ex, vel molestie eros rhoncus vel. Praesent bibendum pretium condimentum. Sed porta ex ex, non semper metus posuere id. Fusce congue luctus enim ac bibendum. Fusce sit amet felis egestas, porttitor eros et, volutpat libero. Morbi gravida iaculis nunc quis rhoncus. Fusce fringilla volutpat ex consequat suscipit.",
-    "version" => "0.0.1")
-    )
-  );
+  function formatMajorContent($h) {
+    $returnStr = "";
+    $id = $h['id'];
+    $title = $h['title'];
+    return "<div class='row major-row'>
+    <div class='section center container' id='content-$id'>
+        <div class='row'>
+          <div class='heading major-heading col'>
+            <span class='major-title'>$title</span>
+            <hr>
+          </div>
+        </div>
+    </div>
+    </div>";
+  }
+
+  function formatMinorContent($h) {
+    $returnStr = "";
+    $content = $h['content'];
+    $id = $h['id'];
+    $title = $h['title'];
+    $text = $h['content'][0]['text'];
+    $version = $h['content'][0]['version'];
+    $returnStr .= "<div class='row minor-row'>
+    <div class='section center container' id='content-$id'>
+        <div class='row'>
+          <div class='heading minor-heading col'>
+            <span class='minor-title'>$title</span>
+            <span class='current-version'>Version: $version</span>
+          </div>
+          <div class='info col'>
+            $text
+          </div>";
+    // if there are older versions
+    if (count($content) > 1) {
+      $returnStr .= "<div class='view-older-versions dropdownButton' data-toggle='old-$id'>View Older Versions <span class='fa fa-caret-down' aria-hidden='true'></span></div>";
+      $returnStr .= "<div id='old-$id' class='old-versions' style='display: none;'>
+      <div class='container'>";
+      foreach ($content as $key => $c) {
+        if ($key != 0) {
+          $version = $c['version'];
+          $text= $c['text'];
+          $returnStr .= "
+          <div class='row'>
+            <div class='col'>
+              <div class='old-version-heading'>
+                <span class='old-version-tag'>Version: $version</span>
+              </div>
+              <div class='info old-info'>
+                $text
+              </div>
+            </div>
+          </div>";
+        }
+      }
+      $returnStr .= "</div>
+      </div>
+      </div>";
+    }
+    // end if there are older versions
+    $returnStr .="</div></div></div>";
+    return $returnStr;
+  }
+  function getUnrefinedDocumentContent($heading) {
+    global $DOC_PAGE_CONTENT;
+    // load the content
+    if (is_null($DOC_PAGE_CONTENT)) {
+      $DOC_PAGE_CONTENT = json_decode(file_get_contents(__DIR__ . "/doc_content.json"), true);
+    }
+    if (!empty($DOC_PAGE_CONTENT[$heading]))
+      return $DOC_PAGE_CONTENT[$heading];
+    return NULL;
+  }
 ?>
--- docs/html/sidebar.php
+++ docs/html/sidebar.php
@@ -1,16 +1,33 @@
 <?php
   function getSidebarLinks($headings) {
     $returnString = "";
-    foreach ($headings as $h) {
-      $title = $h['title'];
-      $id = $h['id'];
-      $returnString .= "<li class='list-group-item' onclick=\"scrollToPageId('content-$id')\">
-                            <span class='sidebar-title'>$title</span> 
-                            <div class='sidebar-pointers'>
-                              <span class='fa fa-chevron-right'></span><span class='fa fa-chevron-right'></span>
-                            </div>
-                        </li>";
+    foreach ($headings as $majorH => $minorHeadings) {
+      $majorIndex = array_search($majorH ,array_keys($headings));
+      // display the major heading
+      $returnString .= getMajorSidebarItem($majorH, $majorIndex);
+      // display the inner content for the major heading
+      foreach ($minorHeadings as $minorIndex => $minorH) {
+        $returnString .= getMinorSidebarItem($minorH, $majorIndex .'-'. $minorIndex);
+      }
     }
     return $returnString;
+  }
+
+  function getMinorSidebarItem($title, $id) {
+    return "<li class='list-group-item sidebar-minor-item' onclick=\"scrollToPageId('content-$id')\">
+        <span class='sidebar-title'>$title</span> 
+        <div class='sidebar-pointers'>
+          <span class='fa fa-chevron-right'></span><span class='fa fa-chevron-right'></span>
+        </div>
+    </li>";
+  }
+
+  function getMajorSidebarItem($title, $id) {
+    return "<li class='list-group-item sidebar-major-item' onclick=\"scrollToPageId('content-$id')\">
+        <span class='sidebar-title'>$title</span> 
+        <div class='sidebar-pointers'>
+          <span class='fa fa-chevron-right'></span><span class='fa fa-chevron-right'></span>
+        </div>
+    </li>";
   }
 ?>
