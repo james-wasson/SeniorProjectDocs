a517b8df493f31bb0d891eb075edba57777514f1
shadebane <shadebane96@gmail.com>
2018-02-19 14:28:12 -0600

    Adding project plan

--- wasson/page.php
+++ wasson/page.php
@@ -2,8 +2,8 @@
   $CONTENT = array();
   $CONTENT['HEAD'] = file_get_contents(__DIR__."/html/includes.html");
   $CONTENT['HEAD'] .= "<script src='./wasson/js/pdf.js'></script>";
-  $CONTENT['HEAD'] .= "<link rel='stylesheet' href='./wasson/css/dropdown.css'></script>";
-  $CONTENT['HEAD'] .= "<link rel='stylesheet' href='./wasson/css/dropContent.css'></script>";
+  $CONTENT['HEAD'] .= "<link rel='stylesheet' href='./wasson/css/dropdown.css'></link>";
+  $CONTENT['HEAD'] .= "<link rel='stylesheet' href='./wasson/css/dropContent.css'></link>";
   $CONTENT['BODY'] = file_get_contents(__DIR__."/html/body.html");
   $CONTENT['EMAIL'] = "jameswasson1@gmail.com"
 ?>
