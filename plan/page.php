31962bd424e275816b53311ea63033f71b6ea6fa
shadebane <shadebane96@gmail.com>
2018-02-21 21:12:07 -0600

    fixing one issue

--- plan/page.php
+++ plan/page.php
@@ -1,6 +1,6 @@
 <?php
   $CONTENT = array();
   $CONTENT['HEAD'] = "<link rel='stylesheet' href='./plan/css/iframe.css'></link>";
-  $CONTENT['HEAD'] .= "<script src='/plan/js/iframe.js'></script>";
+  $CONTENT['HEAD'] .= "<script src='./plan/js/iframe.js'></script>";
   $CONTENT['BODY'] = file_get_contents(__DIR__."/html/body.html");
 ?>
